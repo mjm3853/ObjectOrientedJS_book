cfbbddfa90845eb687a0e6b86ccb31ea3058d8ed
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-04-18 18:59:04 -0400

    small changes

--- sandbox.js
+++ sandbox.js
@@ -5,6 +5,20 @@ function reflect(value) {
 console.log(reflect('poopy'));
 console.log(typeof reflect);
 
-function change(woof) {
-    return woof - 1;
-}
+//--------------------------------
+
+var numbers = [1, 5, 8, 4, 7, 10, 2, 6];
+
+// comparison functions allows you to sort numbers in an array
+numbers.sort(function (first, second) {
+    return first - second;
+});
+
+console.log(numbers);
+
+//--------------------
+
+//default sort does not work because it treats items as strings
+numbers.sort();
+
+console.log(numbers);
