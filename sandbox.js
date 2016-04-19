ca6a0e47b24d344ac94d28503561c552deca04bf
Matt McGrath <matthew.mcgrath12@gmail.com>
2016-04-18 22:07:05 -0400

    spacer log and some more property stuff

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,17 @@
+var spacer = "-----------------";
+
+//------------------------------------
+console.log(spacer);
+
 function reflect(value) {
     return value;
 }
 
-console.log(reflect('poopy'));
+console.log(reflect('reflect'));
 console.log(typeof reflect);
 
 //--------------------------------
+console.log(spacer);
 
 var numbers = [1, 5, 8, 4, 7, 10, 2, 6];
 
@@ -22,12 +28,14 @@ numbers.sort();
 console.log(numbers);
 
 //---------------------------------------
+console.log(spacer);
 
 var person1 = {
     name: "Matt"
 };
 
 person1.age = "Redacted";
+person1.sex = "Yes Please";
 
 //unreliable since this evaluates truthy vs falsy 
 if (person1.age){
@@ -41,13 +49,31 @@ console.log("boolean response of reliably checking property in object: ", reliab
 
 //for just has own property
 var reliableOwnCheck = person1.hasOwnProperty("age");
+
 console.log("boolean response of reliably checking own property in object: ", reliableCheck)
 
 //-------------------------------------------
+console.log(spacer);
 
+//for when you do not need the properties in an array
 var property;
 
 for (property in person1) {
-    console.log("Property Name: " + property);
-    console.log("Property Value: " + person1[property]);
+    console.log("Property name: " + property);
+    console.log("Property value: " + person1[property]);
 }
+
+console.log(spacer);
+
+//for when you want the properties in an array
+var properties = Object.keys(person1);
+
+var i, len;
+
+for (var i = 0, len = properties.length; i < len; i++){
+    console.log("Property array name: " + properties[i]);
+    console.log("Property array value: " + person1[properties[i]]);
+}
+
+//-------------------------------------------
+console.log(spacer);
